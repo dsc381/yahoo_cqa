73bfa655ad7027800984c631b45a655ed8a4d34b
dsc381 <dsc381@nyu.edu>
2015-11-03 00:39:58 -0500

    added indexing correctly

--- trec_convert.py
+++ trec_convert.py
@@ -1,19 +1,27 @@
 import json
+import codecs
+
 f = open("desc_full.json","r")
 json_object = json.load(f)
-q = open("yqa.queries","w")
-a = open("answers.dat","w")
+q = codecs.open("yqa.queries","w", encoding='utf-8')
+a = codecs.open("answers.dat","w", encoding='utf-8')
 q_true = open("yqa.judge","w")
 
 k = 0
 for i in json_object:
     q.write(i["q_uid"]+"\t"+i["question"]+"\n")
+    q_true.write(i["q_uid"] +"\t" + str(k)+"\n")
     a.write("<docno>"+str(k)+"</docno>\n")
-    k += 1
     a.write("<text>\n")
     a.write(i["answer"]+"\n")
     a.write("</text>\n")
+    k += 1
+    repeat = 0
     for ans in i['nbestanswers']:
+        #TODO fix bug and then remove thise
+        if repeat == 0:
+            repeat += 1
+            continue
         a.write("<docno>"+str(k)+"</docno>\n")
         k += 1
         a.write("<text>\n")
